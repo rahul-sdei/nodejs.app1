9422a4b28210564e395347f75f525e48e8af60c3
Rahul Sethi <rahulse@SDEI-OS-S24.(none)>
2015-08-18 12:22:07 +0530

    modify .gitignore to exclude uploads directory

---
+++ main.js
@@ -0,0 +1,5 @@
+mathObj = require('./my_math.js');
+
+TAG = 'Modified value';
+mathObj(3, 12);
+console.log('Tag value: ', TAG);
---
+++ my_math.js
@@ -0,0 +1,6 @@
+var TAG = 'MY_APP';
+
+module.exports = function(param1, param2) {
+    console.log('Tag value: ', TAG);
+    console.log('Sum value: ', (param1 + param2));
+};
